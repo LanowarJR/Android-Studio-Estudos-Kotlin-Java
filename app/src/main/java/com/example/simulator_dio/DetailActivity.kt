d5c55567f2893af71a0b4bdc75222fe8a66f4ab2
Mauro Paulino de Paiva JR <mauro.paulino@gmail.com>
2022-04-10 18:29:42 -0300

    Criação dos layouts das listas e detalhes

---
+++ app/src/main/java/com/example/simulator_dio/DetailActivity.kt
@@ -0,0 +1,19 @@
+package com.example.simulator_dio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simulator_dio.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
